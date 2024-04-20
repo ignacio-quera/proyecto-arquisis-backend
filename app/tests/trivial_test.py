4ad28d6e0e992dcc130ca7b9ae0e17fc548a889d
ignacio-quera <ignacio.quera@uc.cl>
2024-04-20 15:14:46 -0400

    CI changed test structure

--- app/tests/trivial_test.py
+++ app/tests/trivial_test.py
@@ -1,2 +1,14 @@
+<<<<<<< HEAD
 def test_always_fails():
     assert False, "This test always fails"
+=======
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+>>>>>>> 057a9df (CI changed test structure)
