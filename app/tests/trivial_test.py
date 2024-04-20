0eee9d5d9f205bffd9504568e2ba4dd75544c138
ignacio-quera <ignacio.quera@uc.cl>
2024-04-20 15:35:19 -0400

    CI changed test

--- app/tests/trivial_test.py
+++ app/tests/trivial_test.py
@@ -1,9 +1,2 @@
-from fastapi.testclient import TestClient
-from app import app
-
-client = TestClient(app)
-
-def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+def test_always_fails():
+    assert False, "This test always fails"
