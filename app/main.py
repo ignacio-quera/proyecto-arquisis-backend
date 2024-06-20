9756ca1b5e15c7e9ddcef2c41f8900c8a6587fdb
ignacio-quera <ignacio.quera@uc.cl>
2024-06-20 17:32:23 -0400

    feat: setup new-relic

--- app/main.py
+++ app/main.py
@@ -29,5 +29,5 @@ app.include_router(router)
 async def root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
